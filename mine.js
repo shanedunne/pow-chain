a85ef38668ae866867b8f3eaa8440a9217ccd882
shanedunne <shanejjdunne@hotmail.com>
2022-02-03 16:59:32 +0000

    add TARGET_DIFFICULTY variable within mine function to complete functionality

--- mine.js
+++ mine.js
@@ -6,9 +6,11 @@ const db = require('./db');
 const {PUBLIC_KEY} = require('./config');
 const { BlockList } = require('net');
 
-let leadingZero = 4;
-let fCount = 60;
-const TARGET_DIFFICULTY = BigInt("0x" + "0".repeat(leadingZero) + "F".repeat(fCount));
+// set as variables to allow adjustment
+let leadingZero = 3;
+let fCount = 61;
+
+let TARGET_DIFFICULTY = BigInt("0x" + "0".repeat(leadingZero) + "F".repeat(fCount));
 const BLOCK_REWARD = 10;
 
 
@@ -23,13 +25,14 @@ function stopMining() {
     mining = false;
 }
 
+// push block times here
 var blockTimeArr = [];
-var avTimeArr = [];
 
 
 function mine() {
     if(!mining) return;
 
+    // get block start time
     const startTime = Date.now()
 
     const block = new Block();
@@ -49,36 +52,42 @@ function mine() {
 
     console.log(`Block #${db.blockchain.blockHeight()} HASH: 0x${block.hash()} NONCE: ${block.nonce}`)
 
+    // get time at block execution
     let time = Date.now()
+
+    // get block time
     let blockTime = time - startTime
+
+    // push to array containing block times to get average
     blockTimeArr.push(blockTime)
     console.log(blockTime)
 
+    // every 5 blocks run this
     if(blockTimeArr.length % 5 === 0) {
         let averageTime = Math.floor((blockTimeArr.reduce((a, b) => a + b, 0)) / 5)
+
+        // if average less than 6000 increment leadingZero and decrement fCount then empty blockTimeArr to refresh average calculation
         if (averageTime < 6000) {
             leadingZero++;
             fCount--;
             blockTimeArr = []
             console.log("Difficulty Increased")
 
-            console.log(leadingZero)
-            console.log(fCount)
-
+        // if average greater than 14000 decrement leadingZero and increment fCount then empty blockTimeArr to refresh average calculation
         } else if(averageTime > 14000) {
             leadingZero--;
             fCount++;
             blockTimeArr = []
             console.log("Difficulty Decreased")
-
-            console.log(leadingZero)
-            console.log(fCount)
+        
+        // if difficulty within the correct band empty blockTimeArr to refresh average calculation
         } else {
             blockTimeArr = []
             console.log("Difficulty not adjusted")
         }
         console.log(averageTime)
     }
+    TARGET_DIFFICULTY = BigInt("0x" + "0".repeat(leadingZero) + "F".repeat(fCount));
 
     mine()
 
